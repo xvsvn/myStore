3de99aa95eddff925e2b72113ec2a67b928c91f7
xvsvn <86996400+xvsvn@users.noreply.github.com>
2022-05-22 15:53:31 +0500

    Initial Commit

---
+++ myStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myStore
+//
+//  Created by Xasan Xasanov on 22/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
